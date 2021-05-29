9ec2f4a8a33a91f8ae8e8bf0db46ae0c9fd36914
CHChang <holybible934@gmail.com>
2021-05-29 20:27:06 +0800

    Stage 2/6 Create dataType process flow

--- Sorting Tool/task/src/sorting/Main.java
+++ Sorting Tool/task/src/sorting/Main.java
@@ -5,13 +5,37 @@ import java.util.*;
 public class Main {
     public static void main(final String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String dataType = "";
+        if ("-dataType".equals(args[0])) {
+            dataType = args[1];
+        }
+        switch (dataType) {
+            case "line":
+                printLongestLine(scanner);
+                break;
+            case "long":
+                printGreatestLong(scanner);
+                break;
+            case "word":
+            default:
+                printLongestWord(scanner);
+                break;
+        }
+    }
+
+    private static void printLongestWord(Scanner scanner) {
+    }
+
+    private static void printLongestLine(Scanner scanner) {
+    }
+
+    private static void printGreatestLong(Scanner scanner) {
         int totalInput = 0;
         long greatest = Long.MIN_VALUE;
         int counter = 0;
 
         while (scanner.hasNextLong()) {
             long number = scanner.nextLong();
-            // write your code here
             totalInput++;
             if (number > greatest) {
                 greatest = number;
