880362359c2647f861d23f1f68c221660725fc98
CHChang <holybible934@gmail.com>
2021-05-30 14:57:25 +0800

    Stage 2/6 passed 1. Complete printLongestLine() 2. Complete printLongestWord()

--- Sorting Tool/task/src/sorting/Main.java
+++ Sorting Tool/task/src/sorting/Main.java
@@ -24,9 +24,49 @@ public class Main {
     }
 
     private static void printLongestWord(Scanner scanner) {
+        int totalWords = 0;
+        String longestWord = "";
+        int counter = 0;
+        while (scanner.hasNext()) {
+            String inputWord = scanner.next();
+            totalWords++;
+            if (inputWord.length() > longestWord.length()) {
+                longestWord = inputWord;
+                counter = 1;
+            } else if (inputWord.compareTo(longestWord) == 0) {
+                counter++;
+            } else if (inputWord.length() == longestWord.length()
+                    && inputWord.compareTo(longestWord) > 0) {
+                longestWord = inputWord;
+                counter = 1;
+            }
+        }
+        System.out.println("Total words: " + totalWords + ".");
+        System.out.print("The longest word: " + longestWord);
+        System.out.println(" (" + counter + " time(s), " + counter * 100 / totalWords + "%).");
     }
 
     private static void printLongestLine(Scanner scanner) {
+        int totalLines = 0;
+        String longestLine = "";
+        int counter = 0;
+        while (scanner.hasNextLine()) {
+            String inputLine = scanner.nextLine();
+            totalLines++;
+            if (inputLine.length() > longestLine.length()) {
+                longestLine = inputLine;
+                counter = 1;
+            } else if (inputLine.compareTo(longestLine) == 0) {
+                counter++;
+            } else if (inputLine.length() == longestLine.length()
+                    && inputLine.compareTo(longestLine) > 0) {
+                longestLine = inputLine;
+                counter = 1;
+            }
+        }
+        System.out.println("Total lines: " + totalLines + ".\n" + "The longest line:");
+        System.out.println(longestLine);
+        System.out.println("(" + counter + " time(s), " + counter * 100 / totalLines + "%).");
     }
 
     private static void printGreatestLong(Scanner scanner) {
@@ -45,7 +85,6 @@ public class Main {
                 counter++;
             }
         }
-
         System.out.println("Total numbers: " + totalInput + ".");
         System.out.print("The greatest number: " + greatest);
         System.out.println(" (" + counter + " time(s), " + counter * 100 / totalInput + "%).");
