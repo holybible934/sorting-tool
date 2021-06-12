f95c1933399bf17b1e56068756e6e929d270b5bb
holybible934 <holybible934@gmail.com>
2021-06-12 18:56:40 +0800

    Merge all sequences

--- Topics/Merge sort in Java/Merge all sequences/src/Main.java
+++ Topics/Merge sort in Java/Merge all sequences/src/Main.java
@@ -1,5 +1,20 @@
+import java.util.*;
+import java.util.stream.IntStream;
+
 class Main {
     public static void main(String[] args) {
         // put your code here
+        Scanner scanner = new Scanner(System.in);
+        final int lineCount = Integer.parseInt(scanner.nextLine());
+        List<Integer> list = new ArrayList<>();
+        IntStream.range(0, lineCount).forEach(
+            i -> Arrays.stream(scanner.nextLine().split(" "))
+            .skip(1)
+            .mapToInt(Integer::parseInt)
+            .forEach(list::add)
+        );
+        Collections.sort(list);
+        Collections.reverse(list);
+        list.forEach(element -> System.out.print(element + " "));
     }
 }
