7a8ccee614fc7e3313a645c9c94a91a1bc4350e8
holybible934 <holybible934@gmail.com>
2021-06-24 21:20:50 +0800

    Implement sorting in natural order

--- Sorting Tool/task/src/sorting/Main.java
+++ Sorting Tool/task/src/sorting/Main.java
@@ -1,6 +1,10 @@
 package sorting;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Main {
     public enum SORTING_DATA_TYPE {
@@ -33,6 +37,10 @@ public class Main {
                 sortingDataType = SORTING_DATA_TYPE.NATURAL_LINES;
             }
         }
+        List<String> inputLines = new ArrayList<>();
+        while (scanner.hasNextLine()) {
+            inputLines.add(scanner.nextLine());
+        }
         switch (sortingDataType) {
             case BYCOUNT_LINES:
                 break;
@@ -41,87 +49,40 @@ public class Main {
             case BYCOUNT_NUMBERS:
                 break;
             case NATURAL_WORDS:
+                sortingWordsInNatural(inputLines);
                 break;
             case NATURAL_NUMBERS:
+                sortingNumbersInNatural(inputLines);
                 break;
             case NATURAL_LINES:
+                sortLinesInNatural(inputLines);
             default:
                 break;
         }
     }
 
-    private static List<Integer> saveIntegerList(Scanner scanner) {
-        List<Integer> integerList = new ArrayList<>();
-        while (scanner.hasNextInt()) {
-            integerList.add(scanner.nextInt());
-        }
-        return integerList;
+    private static void sortingWordsInNatural(List<String> inputLines) {
+        List<String> inputWords = inputLines.stream().map(line -> line.split("\\s+", 0))
+                .flatMap(Arrays::stream)
+                .sorted().collect(Collectors.toList());
+        System.out.printf("Total words: %d.%n", inputWords.size());
+        System.out.print("Sorted data: ");
+        inputWords.forEach(word -> System.out.print(word + " "));
     }
 
-    private static void printLongestWord(Scanner scanner) {
-        int totalWords = 0;
-        String longestWord = "";
-        int counter = 0;
-        while (scanner.hasNext()) {
-            String inputWord = scanner.next();
-            totalWords++;
-            if (inputWord.length() > longestWord.length()) {
-                longestWord = inputWord;
-                counter = 1;
-            } else if (inputWord.compareTo(longestWord) == 0) {
-                counter++;
-            } else if (inputWord.length() == longestWord.length()
-                    && inputWord.compareTo(longestWord) > 0) {
-                longestWord = inputWord;
-                counter = 1;
-            }
-        }
-        System.out.println("Total words: " + totalWords + ".");
-        System.out.print("The longest word: " + longestWord);
-        System.out.println(" (" + counter + " time(s), " + counter * 100 / totalWords + "%).");
+    private static void sortingNumbersInNatural(List<String> inputLines) {
+        List<Long> inputLongs = inputLines.stream().map(line -> line.split("\\s+", 0))
+                .flatMap(Arrays::stream)
+                .mapToLong(Long::parseLong)
+                .sorted().boxed().collect(Collectors.toList());
+        System.out.printf("Total numbers: %d.%n", inputLongs.size());
+        System.out.print("Sorted data: ");
+        inputLongs.forEach(num -> System.out.print(num + " "));
     }
 
-    private static void printLongestLine(Scanner scanner) {
-        int totalLines = 0;
-        String longestLine = "";
-        int counter = 0;
-        while (scanner.hasNextLine()) {
-            String inputLine = scanner.nextLine();
-            totalLines++;
-            if (inputLine.length() > longestLine.length()) {
-                longestLine = inputLine;
-                counter = 1;
-            } else if (inputLine.compareTo(longestLine) == 0) {
-                counter++;
-            } else if (inputLine.length() == longestLine.length()
-                    && inputLine.compareTo(longestLine) > 0) {
-                longestLine = inputLine;
-                counter = 1;
-            }
-        }
-        System.out.println("Total lines: " + totalLines + ".\n" + "The longest line:");
-        System.out.println(longestLine);
-        System.out.println("(" + counter + " time(s), " + counter * 100 / totalLines + "%).");
-    }
-
-    private static void printGreatestLong(Scanner scanner) {
-        int totalInput = 0;
-        long greatest = Long.MIN_VALUE;
-        int counter = 0;
-
-        while (scanner.hasNextLong()) {
-            long number = scanner.nextLong();
-            totalInput++;
-            if (number > greatest) {
-                greatest = number;
-                counter = 1;
-            }
-            else if (greatest == number) {
-                counter++;
-            }
-        }
-        System.out.println("Total numbers: " + totalInput + ".");
-        System.out.print("The greatest number: " + greatest);
-        System.out.println(" (" + counter + " time(s), " + counter * 100 / totalInput + "%).");
+    private static void sortLinesInNatural(List<String> inputLines) {
+        System.out.printf("Total numbers: %d.%n", inputLines.size());
+        System.out.println("Sorted data: ");
+        inputLines.stream().sorted().forEach(System.out::println);
     }
 }
